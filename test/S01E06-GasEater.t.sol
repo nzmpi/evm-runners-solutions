0b1ea5e75bf9adf14bdeeb00ca4fb31c9e5cb141
beskay <beskay0x@gmail.com>
2024-03-22 09:30:31 +0100

    :zap: More accurate gas reporting for gasEater

--- test/S01E06-GasEater.t.sol
+++ test/S01E06-GasEater.t.sol
@@ -29,48 +29,70 @@ contract GasEaterTestBase is Test {
     }
 
     function test_s01e06_sanity() public {
-        // check gas usage
-        uint256 gasBefore = gasleft();
-        gasEater.eatGas();
-        uint256 gasAfter = gasleft();
-        console2.log("Gas used for 'eatGas':", gasBefore - gasAfter - 5009);
+        uint256 gasBefore;
+        uint256 gasAfter;
+        address addr = address(gasEater);
+
+        assembly {
+            pop(call(gas(), addr, 0, 0, 0, 0, 0))
+        }
+
+        assembly {
+            mstore(0x00, 0x83f157b1) // Start at 0x1c (28), this is the first calldata entry.     v -> 0x1c
+            // 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 83 f1 57 b1
+            gasBefore := gas()
+            pop(call(gas(), addr, 0, 0x1c, 0x04, 0, 0))
+            gasAfter := gas()
+        }
+        console2.log("Gas used for 'eatGas':", gasBefore - gasAfter - 129);
         console2.log("Gas target for 'eatGas':", uint256(719));
-        int256 gasMismatch = int256(gasBefore) - int256(gasAfter) - int256(5009) - int256(719);
-        console2.log("Gas missmatch:", gasMismatch);
+        int256 gasMismatch = int256(gasBefore) - int256(gasAfter) - int256(129) - int256(719);
+        console2.log("Gas mismatch:", gasMismatch);
         assertEq(gasMismatch, 0);
 
         // empty line
         console2.log("");
 
-        // check gas usage
-        gasBefore = gasleft();
-        gasEater.eatMoreGas();
-        gasAfter = gasleft();
-        console2.log("Gas used for 'eatMoreGas':", gasBefore - gasAfter - 503);
+        assembly {
+            mstore(0x00, 0x651f221d)
+            gasBefore := gas()
+            pop(call(gas(), addr, 0, 0x1c, 0x04, 0, 0))
+            gasAfter := gas()
+        }
+        console2.log("Gas used for 'eatMoreGas':", gasBefore - gasAfter - 129);
         console2.log("Gas target for 'eatMoreGas':", uint256(65537));
-        gasMismatch = int256(gasBefore) - int256(gasAfter) - int256(503) - int256(65537);
-        console2.log("Gas missmatch:", gasMismatch);
+        gasMismatch = int256(gasBefore) - int256(gasAfter) - int256(129) - int256(65537);
+        console2.log("Gas mismatch:", gasMismatch);
         assertEq(gasMismatch, 0);
 
         // empty line
         console2.log("");
 
-        // check gas usage
-        gasBefore = gasleft();
-        gasEater.eatEvenMoreGas();
-        gasAfter = gasleft();
-        console2.log("Gas used for 'eatEvenMoreGas':", gasBefore - gasAfter - 500);
+        assembly {
+            mstore(0x00, 0x1bf13571)
+            gasBefore := gas()
+            pop(call(gas(), addr, 0, 0x1c, 0x04, 0, 0))
+            gasAfter := gas()
+        }
+        console2.log("Gas used for 'eatEvenMoreGas':", gasBefore - gasAfter - 129);
         console2.log("Gas target for 'eatEvenMoreGas':", uint256(15_485_863));
-        gasMismatch = int256(gasBefore) - int256(gasAfter) - int256(500) - int256(15_485_863);
-        console2.log("Gas missmatch:", gasMismatch);
+        gasMismatch = int256(gasBefore) - int256(gasAfter) - int256(129) - int256(15_485_863);
+        console2.log("Gas mismatch:", gasMismatch);
         assertEq(gasMismatch, 0);
     }
 
     /// forge-config: default.fuzz.runs = 1
     function test_s01e06_gas(uint256 val) public {
-        gasEater.eatGas();
-        gasEater.eatMoreGas();
-        gasEater.eatEvenMoreGas();
+        address addr = address(gasEater);
+
+        assembly {
+            mstore(0x00, 0x83f157b1)
+            pop(call(gas(), addr, 0, 0x1c, 0x04, 0, 0))
+            mstore(0x00, 0x651f221d)
+            pop(call(gas(), addr, 0, 0x1c, 0x04, 0, 0))
+            mstore(0x00, 0x1bf13571)
+            pop(call(gas(), addr, 0, 0x1c, 0x04, 0, 0))
+        }
     }
 
     function test_s01e06_size() public {
